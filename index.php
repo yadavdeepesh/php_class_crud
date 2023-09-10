b2701984fc51c414dddaa66c50f141e8c06775f7
Shubham Sharma <ssharma86099@gmail.com>
2023-09-11 00:07:42 +0530

    again update index.php file

--- index.php
+++ index.php
@@ -6,4 +6,7 @@ $obj -> insert("students",['student_name'=>'Shubham','age'=>'23','city'=>'mzn'])
 echo "Inserted Result is ::";
 echo "<br>";
 print_r($obj->getResult());
+
+// next functionality is update";
+
 ?>
